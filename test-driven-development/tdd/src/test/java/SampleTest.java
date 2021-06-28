266dc5153f337dfebc00b6e065f40889df79a9c4
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-06-29 03:13:36 +0900

    2장 실습 추가

--- test-driven-development/tdd/src/test/java/SampleTest.java
+++ test-driven-development/tdd/src/test/java/SampleTest.java
@@ -7,7 +7,9 @@ public class SampleTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertThat(five.amount).isEqualTo(10);
+        Dollar product = five.times(2);
+        assertThat(product.amount).isEqualTo(10);
+        product = five.times(3);
+        assertThat(product.amount).isEqualTo(15);
     }
 }
