a3f8e67e8c74e876f46f28fccd10db26ab72082b
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-07-03 00:24:13 +0900

    4장 실습 추가

--- test-driven-development/tdd/README.md
+++ test-driven-development/tdd/README.md
@@ -2,6 +2,10 @@
 
 - [ ] $5 + 10CHF = $10 (환율이 2:1일 경우)
 - [x] $5 x 2 = $10
-- [ ] amount를 private으로 만들기
-- [ ] Dollar 부작용?
+- [x] amount를 private으로 만들기
+- [x] Dollar 부작용?
 - [ ] Money 반올림?
+- [x] equals()
+- [ ] hashCode()
+- [ ] Equal null
+- [ ] Equal object
--- test-driven-development/tdd/src/test/java/SampleTest.java
+++ test-driven-development/tdd/src/test/java/SampleTest.java
@@ -1,16 +1,15 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SampleTest {
 
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertThat(product.amount).isEqualTo(10);
-        product = five.times(3);
-        assertThat(product.amount).isEqualTo(15);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
